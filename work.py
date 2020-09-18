99b5c9bad11d006900b2cb95050709700b0da844
qwertyrar1 <tyrsinartem@gmail.com>
2020-09-18 19:57:03 +0500

    feat: magic numbers work

--- work.py
+++ work.py
@@ -1,4 +1,8 @@
-print("Исходнй размер изображения 1024x600")
+print("Введите исходные размеры изображения")
+width = int(input())
+height = int(input())
+width_percent = 100 # Пусть ширина = 100%
+height_percent = height / (width / width_percent)
 print("Введите то что хотите найти,h-высоту,w-ширину")
 check = str(input())
 if check != "h" and check != "w":
@@ -6,13 +10,13 @@ if check != "h" and check != "w":
 elif check == "h":
     print("Введите новую ширину")
     h_width = int(input())
-    h_height = h_width / 100 * 58.6
+    h_height = h_width / width_percent * height_percent
     print("Высота:")
     print(round(h_height))
 elif check == "w":
     print("Введите новую высоту")
     w_height = int(input())
-    w_width = w_height / 58.6 * 100
+    w_width = w_height / height_percent * width_percent
     print("Ширина:")
     print(round(w_width))
 
