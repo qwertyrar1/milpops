0b2f905700620ce45672136e5077d284fd66b5aa
qwertyrar1 <tyrsinartem@gmail.com>
2020-10-01 00:25:36 +0500

    feat: new_feature work

--- test_work.py
+++ test_work.py
@@ -1,15 +1,17 @@
 import unittest
 import work
-from work import width
-from work import height
 
-class ResizeOrientationTestCase(unittest.TestCase):
+class Resize(unittest.TestCase):
 
-    def test_orientation_vertical(self):
-        self.assertLess(work.orig_width, work.orig_height , msg="Это горизонтальная картинка")
+    def test_resize(self):
+        [w , h] = work.resize(1024 , 786 , "w" , 300)
+        self.assertEqual(w , 300)
+        self.assertEqual(h , 230)
 
-    def test_orientation_horizont(self):
-        self.assertGreater(work.orig_width , work.orig_height , msg="Это вертикальная картинка")
+    def test_resize_2(self):
+        [w, h] = work.resize(1920, 1080, "h", 540)
+        self.assertEqual(w, 960)
+        self.assertEqual(h, 540)
 
 if __name__ == '__main__':
     unittest.main()
--- work.py
+++ work.py
@@ -1,36 +1,14 @@
-print("Введите исходные размеры изображения")
-def width():
-    width = int(input())
-    return width
-orig_width = width()
-
-def height():
-    height = int(input())
-    return height
-orig_height = height()
-
-def resize():
+def resize(orig_width, orig_height, check, value):
     width_percent = 100 # Пусть ширина = 100%
     height_percent = orig_height / (orig_width / width_percent)
-    print("Изменить по высоте или ширине?h/w")
-    check = str(input())
-
-    if check != "h" and check != "w":
-        return "Данные введены неверно!"
-    elif check == "w":
-        print("Введите новую ширину")
-        w_width = int(input())
-        w_height = w_width / width_percent * height_percent
-        return round(w_width) , round(w_height)
+    if check == "w":
+        height = value / width_percent * height_percent
+        return round(value) , round(height)
     elif check == "h":
-        print("Введите новую высоту")
-        h_height = int(input())
-        h_width = h_height / height_percent * width_percent
-        return round(h_width) , round(h_height)
+        width = value / height_percent * width_percent
+        return round(width) , round(value)
 
 
-print(resize())
-
-
-
+if __name__ == '__main__':
+    print(resize(1920, 1080, "h", 540))
 
