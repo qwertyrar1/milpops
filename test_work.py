466315d294c0a31cbf069b08baf646cc3364cfe9
qwertyrar1 <tyrsinartem@gmail.com>
2020-09-30 20:48:35 +0500

    feat: new_feature work

--- test_work.py
+++ test_work.py
@@ -1,14 +1,15 @@
 import unittest
+import work
 from work import width
 from work import height
 
 class ResizeOrientationTestCase(unittest.TestCase):
 
     def test_orientation_vertical(self):
-        self.assertLess(width(), height() , msg="Это горизонтальная картинка")
+        self.assertLess(work.orig_width, work.orig_height , msg="Это горизонтальная картинка")
 
     def test_orientation_horizont(self):
-        self.assertGreater(width() , height() , msg="Это вертикальная картинка")
+        self.assertGreater(work.orig_width , work.orig_height , msg="Это вертикальная картинка")
 
 if __name__ == '__main__':
     unittest.main()
