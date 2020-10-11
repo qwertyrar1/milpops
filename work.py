94b2c8a48bbf2d67b3528fb8e10b04eed8ec9ffa
qwertyrar1 <tyrsinartem@gmail.com>
2020-10-12 00:43:54 +0500

    feat: com-line_arg work

--- work.py
+++ work.py
@@ -1,14 +1,51 @@
-def resize(orig_width, orig_height, check, value):
-    width_percent = 100 # Пусть ширина = 100%
-    height_percent = orig_height / (orig_width / width_percent)
-    if check == "w":
-        height = value / width_percent * height_percent
-        return round(value) , round(height)
-    elif check == "h":
-        width = value / height_percent * width_percent
-        return round(width) , round(value)
+import sys
 
+if __name__ == "__main__":
+    try:
+        assert len(sys.argv) == 3
+    except:
+        print("AssertionError")
+        print("Usage: program [number_width]x[number_height] [w|h]:[new_value]")
+        sys.exit()
+
+param_origwh = sys.argv[1].split('x', 2)
+param_check_value = sys.argv[2].split(':', 2)
+
+width = int(param_origwh[0])
+height = int(param_origwh[1])
+check = param_check_value[0]
+value = int(param_check_value[1])
+
+def resize(width, height, check, value):
+    width = int(param_origwh[0])
+    height = int(param_origwh[1])
+    check = param_check_value[0]
+    value = int(param_check_value[1])
+
+    width_percent = 100  # Пусть ширина = 100%
+    height_percent = height / (width / width_percent)
+
+    if check == "w":
+        new_height = value / width_percent * height_percent
+        return round(value), round(new_height)
+    elif param_check_value[0] == "h":
+        new_width = value / height_percent * width_percent
+        return round(new_width), round(value)
+
+
+def parse_args():
+    try:
+        assert int(param_origwh[0]) > 0
+        assert int(param_origwh[1]) > 0
+        assert param_check_value[0] == 'w' or 'h'
+        assert int(param_check_value[1]) > 0
+        assert len(sys.argv) == 3
+    except:
+        return print("AssertionError" , "\nUsage: program [number_width]x[number_height] [w|h]:[new_value]")
+    else:
+        return resize(width, height, check, value)
 
 if __name__ == '__main__':
-    print(resize(1920, 1080, "h", 540))
+    print(parse_args())
+
 
